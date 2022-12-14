d6640023e4b6c633fc97b14f33c7aa2fca46f2c3
Dominic Chambers <dominic.chambers@gmail.com>
2022-12-14 09:58:09 +0000

    Fix build issues

--- packages/oxymora-todomvc/vite.config.ts
+++ packages/oxymora-todomvc/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
-import react from 'plugin-react-swc-emotion' // TODO: revert to official `plugin-react-swc-emotion` once support for configuring `importSource` is added
+// TODO: revert to official `@vitejs/plugin-react-swc` once support for configuring `importSource` is added
+import react from 'plugin-react-swc-emotion' // @ts-ignore
 
 // https://vitejs.dev/config/
 export default defineConfig({
